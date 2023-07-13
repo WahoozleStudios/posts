a586a51f617d5eb44bb0feb7cca323eb0953003a
zac-moon <zacemoon@gmail.com>
2023-07-13 18:38:28 +0100

    fixed

--- app.py
+++ app.py
@@ -5,7 +5,8 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return render_template('index.html')
+    with app.app_context():
+        return render_template('index.html')
 
 @app.route('/login')
 def login():
