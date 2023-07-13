7b2f946081ff3cb0fd51d5981b45d1557349aeb1
zac-moon <zacemoon@gmail.com>
2023-07-13 19:37:57 +0100

    fixed all

--- app.py
+++ app.py
@@ -11,5 +11,8 @@ def base():
 def login():
     return render_template('login.html')
 
+@app.route('/signup')
+def signup():
+    return render_template('signup.html')
 if __name__ == '__main__':
     app.run()
