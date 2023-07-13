b75b7fbdedaa86facd9543fe735918a1d2b54bcb
zac-moon <zacemoon@gmail.com>
2023-07-13 19:23:09 +0100

    fixed

--- app.py
+++ app.py
@@ -5,24 +5,11 @@ app = Flask(__name__)
 
 @app.route('/')
 def base():
-    with app.app_context():
-        return render_template('index.html')
+    return render_template('home.html')
 
 @app.route('/login')
 def login():
     return render_template('login.html')
-
-@app.route('/signup')
-def signup():
-    return render_template("signup.html")
-
-@app.route('/help')
-def help():
-    return render_template('help.html')
-
-@app.route('/home')
-def home():
-    return render_template('home.html')
 
 if __name__ == '__main__':
     app.run()
