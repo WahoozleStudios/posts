2475378d9058a1f33817516f50fc78a3b5a976ce
zac-moon <zacemoon@gmail.com>
2023-07-13 21:07:31 +0100

    fixed loads

--- app.py
+++ app.py
@@ -14,5 +14,17 @@ def login():
 @app.route('/signup')
 def signup():
     return render_template('signup.html')
+
+@app.route('/u/zacmoon')
+def u_zacmoon():
+    return "Zac Moon"
+@app.route('/u')
+def u():
+    return render_template('redirects/u.html')
+
+@app.errorhandler(404)
+def page_not_found(error):
+    return render_template('errors/404.html'), 404
+
 if __name__ == '__main__':
     app.run()
