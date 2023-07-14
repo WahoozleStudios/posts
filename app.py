3321cbb9066251273b5cce75eebc268fcf8b3d88
zac-moon <zacemoon@gmail.com>
2023-07-14 11:29:05 +0100

    fixed directory

--- app.py
+++ app.py
@@ -4,15 +4,15 @@ app = Flask(__name__)
 
 @app.route('/')
 def base():
-    return render_template('home.html')
+    return render_template('home/home.html')
 
 @app.route('/login')
 def login():
-    return render_template('login.html')
+    return render_template('login/login.html')
 
 @app.route('/signup')
 def signup():
-    return render_template('signup.html')
+    return render_template('signup/signup.html')
 
 @app.route('/u')
 def u():
