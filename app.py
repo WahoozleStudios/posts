3aa6b8012048902b6560afaf54800f7b45726ffe
zac-moon <zacemoon@gmail.com>
2023-07-14 11:24:02 +0100

    fixed

--- app.py
+++ app.py
@@ -23,4 +23,4 @@ def page_not_found(error):
     return render_template('errors/404.html'), 404
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=80)
+    app.run(host='0.0.0.0', port=5000)
